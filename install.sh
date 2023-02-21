fad4473ca3e75f63418e59218d9d3a7968b8e0da
Xavier T <xavier.torres.alonso@gmail.com>
2023-02-22 00:47:31 +0100

    install error in new ami

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ source ./env.sh
 ## END PARAMETERS ##
 #Already in app_dir and already appuser 
 mkdir .virtualenvs
-python -m venv "./.virtualenvs/${env_name}"
+python3 -m venv "./.virtualenvs/${env_name}"
 source "./.virtualenvs/${env_name}/bin/activate"
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 #Create cron
 crontab_line="@reboot ${PWD}/${run_script}"
 (crontab -l 2>/dev/null; echo "$crontab_line")| crontab -
@@ -14,7 +14,7 @@ crontab_line="@reboot ${PWD}/${run_script}"
 echo "#!/bin/bash" > "$run_script"
 echo "cd ${PWD}" >> "$run_script"
 echo "source ./.virtualenvs/${env_name}/bin/activate" >> "$run_script"
-echo "nohup python ./${site_name}/manage.py runserver 0.0.0.0:${port} &" >> "$run_script"
+echo "nohup python3 ./${site_name}/manage.py runserver 0.0.0.0:${port} &" >> "$run_script"
 #Installed now run app 
 chmod +x $run_script 
 ./$run_script 
